5d3dbfe1dd7ea006311f18c14b53b758f7e1a9b4
brgerig <brgerig@taylor.edu>
2024-04-09 16:37:25 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class sentinelone (
-  String management_token = $sentinelone::params::management_token
+  String $management_token = $sentinelone::params::management_token
 ) inherits sentinelone::params
 {
   package { 'SentinelAgent'
