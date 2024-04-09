c4dc5edaab4b24af3d553e27c23a57bfe0870552
brgerig <brgerig@taylor.edu>
2024-04-09 16:36:20 -0400

    Delete init.pp

--- init.pp
+++
@@ -1,9 +0,0 @@
-class sentinelone (
-  String management_token = $sentinelone::params::management_token
-) inherits sentinelone::params
-{
-  package { 'SentinelAgent'
-    ensure => latest,
-    require => Class['subscription_manager'],
-  }
-}
